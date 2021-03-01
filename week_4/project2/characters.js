f9d3dcac66781907cf31d7230d80c408e28b4e34
tyrawalker <tyra.walker@bryanuniversity.edu>
2021-03-01 18:42:45 -0500

    week4 project 2 redo

--- week_4/project2/characters.js
+++ week_4/project2/characters.js
@@ -1,20 +1,20 @@
 
 
-document.getElementById("char").addEventListener("onclick",function (){
-
+document.getElementById("char").addEventListener("click",function (){
     const getCharacters= ()=> {
         axios.get("https://rickandmortyapi.com/api/character")
         .then (res => {
-            renderGetCharacters (res.data)
+            renderGetCharacters (res.data.results)
         })
         .catch (err => console.log (err))
     }
-    
+    getCharacters()
+
     const renderGetCharacters= (data => {
         const characterContainer = document.getElementById("characters")
     
         for (let i=0; i<data.length; i++){
-            let character = document.createElement("h1")
+            let character = document.createElement("li")
             character.textContent = data[i].name
             characterContainer.appendChild(character)
         }
--- week_4/project2/episodes.js
+++ week_4/project2/episodes.js
@@ -1,21 +1,21 @@
 
-document.getElementById("epi").addEventListener("onclick", function(){
-
+document.getElementById("epi").addEventListener("click", function(){
     const getEpisodes= ()=> {
         axios.get("https://rickandmortyapi.com/api/episode")
         .then (res => {
-            renderGetEpisodes (res.data)
+            renderGetEpisodes (res.data.results)
         })
         .catch (err => console.log (err))
     }
-    
+    getEpisodes()
+
     const renderGetEpisodes= (data => {
         const episodeContainer = document.getElementById("episodes")
     
         for (let i=0; i<data.length; i++){
             let episode = document.createElement("h1")
             episode.textContent = data[i].name
-            episodeContainer.appendChild(character)
+            episodeContainer.appendChild(episode)
         }
     })
 })
