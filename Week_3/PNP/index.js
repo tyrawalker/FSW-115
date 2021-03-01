881ac49ef9c2f3e34e34885b6e86092c33ea3fc0
tyrawalker <tyra.walker@bryanuniversity.edu>
2021-03-01 17:43:32 -0500

    week3 assignment1 redo

--- Week_3/PNP/index.js
+++ Week_3/PNP/index.js
@@ -6,15 +6,17 @@ xhr.send()
 xhr.onreadystatechange = function(){
     if (xhr.readyState === 4 && xhr.status === 200){
         const JSONData = xhr.responseText
-        const data = JSON.parse (JSONData)
-        console.log (data)
+        const data = JSON.parse(JSONData)
+        console.log (data.objects[0].pokemon)
+        showData(data.objects[0].pokemon)
     }
 }
 
 function showData (arr){
     for (let i= 0; i<arr.length; i++){
         const h1 = document.createElement("h1")
-        h1.textContent= arr[i].name
+        h1.textContent= arr[i].name + "" + arr[i].resource_uri
+        h1.style.color = "blue"
         document.body.appendChild(h1)
     }
 }
